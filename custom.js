7f0d52ad33f786c4af86b7da95d0c5de1e570342
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-02-11 18:10:09 -0500

    Updates the script

--- custom.js
+++ custom.js
@@ -3,11 +3,11 @@ function processExternalAnchors() {
     var $a = $(this);
 
     // don't apply to links that have youtube images
-    if ($a.find('img[src^="http://i1.ytimg.com"]').length) {
+    if ($a.find('img[src^="http"]').length) {
       return false;
     }
 
-    if ($a.attr('data-no-external') === 'true') {
+    if ($a.attr('data-no-external')) {
       return false;
     }
 
